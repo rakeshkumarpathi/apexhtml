f718c66453cf81ca9358b7ca37d55814e7dc0930
rakesh kumar pathi <rakeshkumarpathi7@gmail.com>
2025-07-15 20:47:22 +0530

    Update task-2 content

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const checkboxes = document.querySelectorAll('.todo-list input[type="checkbox"]');
-        const doneBtn = document.getElementById('doneBtn');
-        const celebration = document.getElementById('celebration');
+// Access elements
+var checkboxes = document.querySelectorAll('.todo-list input[type="checkbox"]');
+var doneBtn = document.getElementById('doneBtn');
+var celebration = document.getElementById('celebration');
 
-        function checkTasks() {
-            const allChecked = Array.from(checkboxes).every(cb => cb.checked);
-            doneBtn.disabled = !allChecked;
+// Function to check if all tasks are done
+function checkTasks() {
+    var allChecked = true;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (!checkboxes[i].checked) {
+            allChecked = false;
+            break;
         }
+    }
+    doneBtn.disabled = !allChecked;
+}
 
-        checkboxes.forEach(cb => cb.addEventListener('change', checkTasks));
+// Add event to each checkbox
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', checkTasks);
+}
 
-        doneBtn.addEventListener('click', () => {
-            celebration.style.display = 'block';
-            doneBtn.style.display = 'none';
-        });
+// When Done button is clicked
+doneBtn.addEventListener('click', function() {
+    celebration.style.display = 'block';
+    doneBtn.style.display = 'none';
+});
+
+
+
+// const checkboxes = document.querySelectorAll('.todo-list input[type="checkbox"]');
+//         const doneBtn = document.getElementById('doneBtn');
+//         const celebration = document.getElementById('celebration');
+
+//         function checkTasks() {
+//             const allChecked = Array.from(checkboxes).every(cb => cb.checked);
+//             doneBtn.disabled = !allChecked;
+//         }
+
+//         checkboxes.forEach(cb => cb.addEventListener('change', checkTasks));
+
+//         doneBtn.addEventListener('click', () => {
+//             celebration.style.display = 'block';
+//             doneBtn.style.display = 'none';
+//         });
